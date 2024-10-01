1365944b3475d1d68f799df96a94c97cf97dc348
Ruichao Wu <ruichao.wu@ipa.fraunhofer.de>
2024-10-01 08:38:29 +0200

    add deployment artifact

---
+++ ROSDeploymentExample/src-gen/deploy_ur_applications/ur_driver/ur_driver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+apt install ros-humble-controller-manager --no-install-recommends -qq -y
+apt install ros-humble-robot-state-publisher --no-install-recommends -qq -y
+apt install ros-humble-ur-robot-driver --no-install-recommends -qq -y
